ff932b806c5e420cadf91d2e2c99e876bbca6b6b
“sebastiangrab73@gmail.comgit commit -m Stage <sebastiangrab73@gmail.com>
2024-05-02 12:11:13 +0200

    Update

--- src/Verhandlung.java
+++ src/Verhandlung.java
@@ -1,71 +1,49 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 
-
-//SIMULATION!
-
-/*
- * Was ist das "Problem" der nachfolgenden Verhandlung?
- * - Frühe Stagnation, da die Agenten frühzeitig neue Contracte ablehnen
- * - Verhandlung ist nur für wenige Agenten geeignet, da mit Anzahl Agenten auch die Stagnationsgefahr wächst
- * 
- * Aufgabe: Entwicklung und Anaylse einer effizienteren Verhandlung. Eine Verhandlung ist effizienter, wenn
- * eine frühe Stagnation vermieden wird und eine sozial-effiziente Gesamtlösung gefunden werden kann.
- * 
- * Ideen:
- * - Agenten müssen auch Verschlechterungen akzeptieren bzw. zustimmen, die einzuhaltende Mindestakzeptanzrate wird vom Mediator vorgegeben
- * - Agenten schlagen alternative Kontrakte vor
- * - Agenten konstruieren gemeinsam einen Kontrakt
- * - In jeder Runde werden mehrere alternative Kontrakte vorgeschlagen
- * - Alternative Konstruktionsmechanismen für Kontrakte
- * - Ausgleichszahlungen der Agenten (nur möglich, wenn beide Agenten eine monetaere Zielfunktion haben
- * 
- */
-
-
 public class Verhandlung {	
 
 		public static void main(String[] args) {
 			int[] contract, proposal;
-			Agent agA, agB, agC, agD, agE;
+			Agent agA;
+			Agent agB;
+			Agent agC;
+			Agent agD;
+			Agent agE;
 			Mediator med;
 			int maxRounds, round;
 			boolean voteA, voteB, voteC, voteD, voteE;
-			
-			
-			
-			
-			
-			
+
 			
 			try{
-				agA = new SupplierAgent(new File("data/daten3ASupplier_200.txt"));
-				agB = new CustomerAgent(new File("data/daten4BCustomer_200_5.txt"));
+
+				agA = new AnnealingSupplierAgent(new File("data/daten3ASupplier_200.txt"));
+				agB = new AnnealingCustomerAgent(new File("data/daten4BCustomer_200_5.txt"));
+//				agA = new SupplierAgent(new File("data/daten3ASupplier_200.txt"));
+//				agB = new CustomerAgent(new File("data/daten4BCustomer_200_5.txt"));
 //				agC = new SupplierAgent(new File("data/daten5ASupplier_200.txt"));
 //				agD = new CustomerAgent(new File("data/daten3BCustomer_200_20.txt"));
 //				agE = new CustomerAgent(new File("data/daten4BCustomer_200_5.txt"));
-//				
-				
-				
+
 				med = new Mediator(agA.getContractSize(), agB.getContractSize());
 				
-				//Verhandlung initialisieren
-				contract  = med.initContract();							//Vertrag=Lösung=Jobliste
-				maxRounds = 1000000;										//Verhandlungsrunden
+				// Verhandlung initialisieren
+				contract  = med.initContract();							// VertragslÃ¶sung Jobliste
+				maxRounds = 1000000;										// Verhandlungsrunden
 				//ausgabe(agA, agB, 0, contract);
 				
-				//Verhandlung starten	
-				/*
-				 * 
-				 */
-				for(round=1;round<maxRounds;round++) {					//Mediator				
-					proposal = med.constructProposal(contract);			//zweck: Win-win
-					voteA    = agA.vote(contract, proposal);            //Autonomie + Private Infos
+				// Verhandlung starten
+
+				for(round=1;round<maxRounds;round++) {					// Mediator
+					proposal = med.constructProposal_SHIFT(contract);	// Zweck: Win-win
+					voteA    = agA.vote(contract, proposal);            // Autonomie + Private Infos
 					voteB    = agB.vote(contract, proposal);
-//					voteC    = agC.vote(contract, proposal);            //Autonomie + Private Infos
+//					voteC    = agC.vote(contract, proposal);            // Autonomie + Private Infos
 //					voteD    = agD.vote(contract, proposal);
-//					voteE    = agE.vote(contract, proposal);            //Autonomie + Private Infos
-//					
+//					voteE    = agE.vote(contract, proposal);            // Autonomie + Private Infos
+//
+//					voteB = true;
+
 //					if(voteA && voteB && voteC && voteD && voteE) {
 					if(voteA && voteB) {
 						contract = proposal;
